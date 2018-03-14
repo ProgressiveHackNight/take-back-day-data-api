6ab9d3a9ee741098cc94d4b5efde18c173d1ff23
michel <mb@infoloom.com>
2018-03-14 05:35:10 -0400

    adding a help file

--- location/urls.py
+++ location/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from location import views
+from django.views.generic import TemplateView
+
 
 
 urlpatterns = [
+    url(r'^help/$', TemplateView.as_view(template_name='help.html')),
     url(r'^delete_database/$', views.delete_database, name='delete-database'),
     url(r'^read_json/$', views.read_dec_data_json, name='read-dec-data'),
     url(r'^read_spreadsheet/$', views.read_spreadsheet, name='read-spreadsheet'),
